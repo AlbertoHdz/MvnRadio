7c309651fc238b73560fe2aaef488b51a837a769
Albert Hdz <alberto_8565@hotmail.com>
2024-07-30 23:18:13 -0600

    modificación rutas
    se modifican para que funcionen localmente

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -5,11 +5,21 @@ $(function(){
 });
 
 function ajaxContent(url){
+    console.log(url);
+    let host = window.location.hostname;
+    let path = window.location.pathname;
+    
+    if(host.indexOf("local")>= 0){
+        //if(path.indexOf("mvn") < 0 ){
+            url = "/mvnRadio"+url;
+        //}
+    }
+    console.log("url nueva: ");
+    console.log(url);
     $.ajax({
         url: url,
         type: "GET",
         success: function(data){
-            console.log(url);
             $("#ajax-content").html(data); 
         },
         error: function(e1,e2,e3){
@@ -18,8 +28,9 @@ function ajaxContent(url){
     });
 }
 
-$(document).off("click",".menu-item a");
-$(document).on("click",".menu-item a",function(){
+$(document).off("click","a");
+$(document).on("click","a",function(e){
+    e.preventDefault();
     var url = $(this).attr("data-url");
     ajaxContent(url);
 });
